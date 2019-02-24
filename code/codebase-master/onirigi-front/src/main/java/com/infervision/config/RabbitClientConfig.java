e8a8826ee515c9abc7c774b8ac7d3943c3f6ba33
menhuan <963633167@qq.com>
2019-02-24 23:24:47 +0800

    配置内容

--- code/codebase-master/onirigi-front/src/main/java/com/infervision/config/RabbitClientConfig.java
+++ code/codebase-master/onirigi-front/src/main/java/com/infervision/config/RabbitClientConfig.java
@@ -18,62 +18,62 @@ import static com.infervision.constants.Constants.*;
 public class RabbitClientConfig {
 
 
-    @Autowired
-    RabbitConfig rabbitConfig;
-
-    @Autowired
-    RabbitProperties rabbitProperties;
-
-    /**
-     * @return org.springframework.amqp.core.Queue
-     * @author fruiqi
-     * @description //TODO 初始化队列，如果队列已存在，则不作任何处理
-     * @date 19-1-9 下午4:37
-     **/
-    @Bean
-    public Queue labelStatisticQueue() {
-        return QueueBuilder.durable(LABEL_FIEL_XML_QUEUE_NAME).build();
-    }
-
-    /**
-     * @return org.springframework.amqp.core.TopicExchange
-     * @author fruiqi
-     * @description //TODO 初始化交换机
-     * @date 19-1-9 下午4:38
-     **/
-    @Bean
-    public TopicExchange topicLableExchange() {
-        return (TopicExchange) ExchangeBuilder.topicExchange(DEFAULT_TOPIC_TEST_EXCHANGE).durable(true).build();
-    }
-
-    /**
-     * @param labelStatisticQueue 队列名称
-     * @param topicExchange       绑定的交换机
-     * @return org.springframework.amqp.core.Binding
-     * @author fruiqi
-     * @description 将队列labelStatisticQueue与topicExchange绑定, binding_key为route.label.statistic.queue, 就是完全匹配
-     * @date 19-1-9 下午4:33
-     **/
-    @Bean
-    Binding bindingExchangeLabelStatisticQueue(Queue labelStatisticQueue, TopicExchange topicExchange) {
-        return BindingBuilder.bind(labelStatisticQueue).to(topicExchange).with(LABEL_FIEL_XML_QUEUE_ROUTING_KEY);
-    }
-
-
-    /**
-     * 创建监听器。
-     * @author fruiqi
-     * @date 19-2-11 下午4:18
-     * @param labelStatisticsListener 监听器
-     * @return org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
-     **/
-    @Bean
-    public SimpleMessageListenerContainer mqMessageContainer(LabelStatisticsListener labelStatisticsListener) throws Exception {
-        SimpleMessageListenerContainer container = rabbitConfig.setSimpleMessageListenerContainer(LABEL_FIEL_XML_QUEUE_NAME,
-                true, rabbitProperties.getMaximumDelivery(),
-                rabbitProperties.getConsumer(), AcknowledgeMode.MANUAL, labelStatisticsListener);
-        return container;
-    }
+//    @Autowired
+//    RabbitConfig rabbitConfig;
+//
+//    @Autowired
+//    RabbitProperties rabbitProperties;
+//
+//    /**
+//     * @return org.springframework.amqp.core.Queue
+//     * @author fruiqi
+//     * @description //TODO 初始化队列，如果队列已存在，则不作任何处理
+//     * @date 19-1-9 下午4:37
+//     **/
+//    @Bean
+//    public Queue labelStatisticQueue() {
+//        return QueueBuilder.durable(LABEL_FIEL_XML_QUEUE_NAME).build();
+//    }
+//
+//    /**
+//     * @return org.springframework.amqp.core.TopicExchange
+//     * @author fruiqi
+//     * @description //TODO 初始化交换机
+//     * @date 19-1-9 下午4:38
+//     **/
+//    @Bean
+//    public TopicExchange topicLableExchange() {
+//        return (TopicExchange) ExchangeBuilder.topicExchange(DEFAULT_TOPIC_TEST_EXCHANGE).durable(true).build();
+//    }
+//
+//    /**
+//     * @param labelStatisticQueue 队列名称
+//     * @param topicExchange       绑定的交换机
+//     * @return org.springframework.amqp.core.Binding
+//     * @author fruiqi
+//     * @description 将队列labelStatisticQueue与topicExchange绑定, binding_key为route.label.statistic.queue, 就是完全匹配
+//     * @date 19-1-9 下午4:33
+//     **/
+//    @Bean
+//    Binding bindingExchangeLabelStatisticQueue(Queue labelStatisticQueue, TopicExchange topicExchange) {
+//        return BindingBuilder.bind(labelStatisticQueue).to(topicExchange).with(LABEL_FIEL_XML_QUEUE_ROUTING_KEY);
+//    }
+//
+//
+//    /**
+//     * 创建监听器。
+//     * @author fruiqi
+//     * @date 19-2-11 下午4:18
+//     * @param labelStatisticsListener 监听器
+//     * @return org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+//     **/
+//    @Bean
+//    public SimpleMessageListenerContainer mqMessageContainer(LabelStatisticsListener labelStatisticsListener) throws Exception {
+//        SimpleMessageListenerContainer container = rabbitConfig.setSimpleMessageListenerContainer(LABEL_FIEL_XML_QUEUE_NAME,
+//                true, rabbitProperties.getMaximumDelivery(),
+//                rabbitProperties.getConsumer(), AcknowledgeMode.MANUAL, labelStatisticsListener);
+//        return container;
+//    }
 
 
 }
