cfb1262b83a81e79520570dd68642b03353542de
menhuan <963633167@qq.com>
2019-07-31 23:45:20 +0800

    elaseticsearch

--- code/codebase-master/onirigi-repile/src/main/java/com/infervision/service/impl/ProfessorServiceImpl.java
+++ code/codebase-master/onirigi-repile/src/main/java/com/infervision/service/impl/ProfessorServiceImpl.java
@@ -40,9 +40,9 @@ public class ProfessorServiceImpl implements ProfessorService {
     @Override
     public JSONObject professorService(String url) {
         Map<String, String> headMap = new HashMap<>(10);
-        headMap.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0");
-        headMap.put("Referer", "https://wx.zsxq.com/dweb2/");
-        headMap.put("cookie", "zsxq_access_token=E097E32C-3596-628E-7920-00BD429129F1");
+        headMap.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0");
+        headMap.put("Referer", "https://wx.zsxq.com/dweb2/index/group/init");
+        headMap.put("cookie", "zsxq_access_token=E1D5685F-FDAE-56BD-5F77-64025DFF2EE8");
         RequestUtil requestUtil = new RequestUtil();
         String res = requestUtil.restStar(headMap, url);
         JSONObject jsonObject = JSON.parseObject(res);
