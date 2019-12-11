d2b2fd26bb32c42810a52a49e8d77da810227085
fruiqi <fruiqi@infervision.com>
2019-12-11 21:26:59 +0800

    file study

--- code/python/python_study/eight/file_learn.py
+++ code/python/python_study/eight/file_learn.py
@@ -14,5 +14,42 @@ content = re.read()
 print(content)
 
 
+path = "write.txt"
+write_file = open(path,"w+")
+write_file.write("i like Python,are you ?\n")
+write_file.flush()
+write_file.write("write Python")
+write_file.close()
+read_file = open(path,"r+")
+for line in read_file:
+    print(line)
+read_file.close()
 
 
+import fileinput
+
+path= "code\python\python_study\eight\learn.txt"
+file_obj = fileinput.input(path)
+
+for line in file_obj:
+    print(line)
+
+
+path= "code\python\python_study\eight\learn.txt"
+   
+try:
+    learn = open(path,"r+")
+    content = learn.read()
+    print(content)
+except IOError as e:
+    print("异常",e) 
+finally:
+    learn.close()
+
+with open(path,"r+") as learn :
+    try:
+        content = learn.read()
+        print(content)
+    except IOError as e:
+        print("异常",e)
+    
