6951c67487effddbbc9f7cac0c4a30deba792016
fruiqi <fruiqi@infervision.com>
2020-01-18 12:54:07 +0800

    douban

---
+++ code/python/python_study/fourteen/douban_learn.py
@@ -0,0 +1,8 @@
+# /html/body/div[3]/div[1]/div/div[1]/div[4]/div[1]
+# //*[@id="comments"]
+
+# //*[@id="comments"]/div[1]
+
+import requests
+first_url = ="https://movie.douban.com/subject/26363254/comments?status=P"
+response = requests.get(first_url)
