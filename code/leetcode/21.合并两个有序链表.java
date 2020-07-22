ace61430211d3541235274bd3b69285e73c950be
menhuan <963633167@qq.com>
2020-07-23 07:55:15 +0800

    合并两个有序链表

--- code/leetcode/21.合并两个有序链表.java
+++ code/leetcode/21.合并两个有序链表.java
@@ -17,6 +17,23 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode result1=new ListNode(-1);
+        ListNode result =result1;
+        
+        while(l1!=null && l2 !=null){
+            if (l1.val <=l2.val){
+                result.next=l1;
+                l1=l1.next;
+              
+            }else{
+                result.next=l2;
+                l2=l2.next;
+            }
+            result = result.next;
+        }    
+        result.next = l1 ==null ? l2:l1 ;
+
+        return result1.next;
         
     }
 }
