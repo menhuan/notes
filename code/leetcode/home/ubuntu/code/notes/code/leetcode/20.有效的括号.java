9ceeab8ddc4c61adcae71a519bf2e8fe4482dcb4
ubuntu <ubuntu@localhost.localdomain>
2021-04-27 21:11:00 +0800

    权限问题

---
+++ code/leetcode/home/ubuntu/code/notes/code/leetcode/20.有效的括号.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Map<>
+    }
+}
+// @lc code=end
+
