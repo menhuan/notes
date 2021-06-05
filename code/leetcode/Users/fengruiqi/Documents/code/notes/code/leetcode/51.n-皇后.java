b6e6270b0fe5907f4d860274acdaa2d6d75eefa2
fruiqi <963633167@qq.com>
2021-06-05 15:05:10 +0800

    n皇后问题

--- code/leetcode/Users/fengruiqi/Documents/code/notes/code/leetcode/51.n-皇后.java
+++ code/leetcode/Users/fengruiqi/Documents/code/notes/code/leetcode/51.n-皇后.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -21,7 +22,9 @@ class Solution {
         column = new HashSet<>();
         // 在没有在撇里面需要判断 行+column 和行- column在没在pie里面
         pie = new HashSet<>();
-
+        int[] queues = new int[n];
+        Arrays.fill(queues, -1);
+        dfs(0, n, queues);
         return ans;
     }
 
@@ -31,13 +34,14 @@ class Solution {
      * @param n      最大的行数
      * @param indexs 存储放的皇后的n行 n 列
      */
-    public void dfs(int cur, int n, List<Integer> indexs) {
+    public void dfs(int cur, int n, int[] queues) {
 
         if (cur == n) {
             // 生成棋盘
+            ans.add(productBoard(queues, n));
             return;
         }
-        // process
+        // process 对列进行遍历 上面的cur是行，在dfs中的时候递增行即可
         for (int index = 0; index < n; index++) {
 
             // 如果这一列在上面set中那么就终止本次的循环
@@ -51,16 +55,17 @@ class Solution {
                 continue;
             }
             // 将其加入到棋盘中
-            index.add(index);
+            queues[cur] = index;
 
             // 在规则中都加上该数据
             column.add(index);
             pie.add(cur + index);
             na.add(cur - index);
 
-            dfs(cur + 1, n, indexs);
-            // 将其数据移出出来
-            index.remove(index.size() - 1);
+            dfs(cur + 1, n, queues);
+            // 将其数据移出出来 下面是进行回溯计算 将插入的数据移出来
+            queues[cur] = -1;
+
             column.remove(index);
             pie.remove(cur + index);
             na.remove(cur - index);
@@ -70,16 +75,24 @@ class Solution {
     }
 
     /**
-     * 数组的index代表一个行 value代表队的是列
+     * 数组queue 代表一个行 value代表这一行的列位置
      * 
-     * @param indexs
+     * @param queues 放的皇后的位置
+     * @param n      代表的是棋盘的大小
      * @return
      */
-    public String productBoard(List<Integer> indexs) {
+    public List<String> productBoard(int[] queues, int n) {
         List<String> board = new ArrayList();
-        for (int index = 0; index < indexs.size(); index++) {
-
+        // 生成
+        for (int index = 0; index < n; index++) {
+            // 一行数据
+            char[] empty = new char[n];
+            // 填充.
+            Arrays.fill(empty, '.');
+            empty[queues[index]] = 'Q';
+            board.add(new String(empty));
         }
+        return board;
     }
 
 }
