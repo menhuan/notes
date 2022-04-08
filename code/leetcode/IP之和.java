06c5fbaae7b1a4575fb28de8f597f0570e56d2e1
ruiqirq.feng <ruiqirq.feng@qunar.com>
2022-04-08 09:51:07 +0800

    提交爬虫数据

---
+++ code/leetcode/IP之和.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String validIPAddress(String queryIP) {
+        if (queryIP.indexOf('.') >= 0) {
+            // IPv4
+            int last = -1;
+            for (int i = 0; i < 4; ++i) {
+                int cur = (i == 3 ? queryIP.length() : queryIP.indexOf('.', last + 1));
+                if (cur < 0) {
+                    return "Neither";
+                }
+                if (cur - last - 1 < 1 || cur - last - 1 > 3) {
+                    return "Neither";
+                }
+                int addr = 0;
+                for (int j = last + 1; j < cur; ++j) {
+                    if (!Character.isDigit(queryIP.charAt(j))) {
+                        return "Neither";
+                    }
+                    addr = addr * 10 + (queryIP.charAt(j) - '0');
+                }
+                if (addr > 255) {
+                    return "Neither";
+                }
+                if (addr > 0 && queryIP.charAt(last + 1) == '0') {
+                    return "Neither";
+                }
+                if (addr == 0 && cur - last - 1 > 1) {
+                    return "Neither";
+                }
+                last = cur;
+            }
+            return "IPv4";
+        } else {
+            // IPv6
+            int last = -1;
+            for (int i = 0; i < 8; ++i) {
+                int cur = (i == 7 ? queryIP.length() : queryIP.indexOf(':', last + 1));
+                if (cur < 0) {
+                    return "Neither";
+                }
+                if (cur - last - 1 < 1 || cur - last - 1 > 4) {
+                    return "Neither";
+                }
+                for (int j = last + 1; j < cur; ++j) {
+                    if (!Character.isDigit(queryIP.charAt(j)) && !('a' <= Character.toLowerCase(queryIP.charAt(j)) && Character.toLowerCase(queryIP.charAt(j)) <= 'f')) {
+                        return "Neither";
+                    }
+                }
+                last = cur;
+            }
+            return "IPv6";
+        }
+    }
+}
--- python/taobao/filter.py
+++ python/taobao/filter.py
@@ -4,53 +4,72 @@ from numpy import single
 import pandas
 
 def filter_ci(excel_data,blacklist,blacklist_guanjianci,contains_cis):
+    excel_data = excel_data.drop(excel_data[excel_data['蓝海值']=='蓝海值'].index )
     excel_data['蓝海值'].replace('∞','0',inplace=True)
-    excel_data['蓝海值'] = excel_data['蓝海值'].astype('int')
+    excel_data['蓝海值'] = excel_data['蓝海值'].astype('float')
 
     # 第一个过滤方式 计算
     filter_data = excel_data[(excel_data.在线商品数 > 50)
                              &(excel_data.支付转化率 > 3) 
                              &(excel_data.商城点击占比 < 60)
-                             &(excel_data.蓝海值 > 1.0 )]
-    # 过滤一些黑名单关键词
-    filter_data = filter_data[~filter_data['关键词'].isin(blacklist)]
+                             &(excel_data.蓝海值 > 5.0 )
+                             &(excel_data.搜索人气 > 7000)]
+    filter_data.to_excel("filter.xlsx",sheet_name="在开始过滤前的数据")
+    with pandas.ExcelWriter("filter.xlsx", mode='a', engine='openpyxl') as writer:
+        # 过滤一些黑名单关键词
+        filter_data[filter_data['关键词'].isin(blacklist)].to_excel(writer,sheet_name="blacklist过滤")
+        filter_data = filter_data[~filter_data['关键词'].isin(blacklist)]
     
-    # 过滤长度 
-    filter_data = filter_data[filter_data['关键词'].str.len() < 20 ] 
-    # 过滤某些词
-    filter_balck_ci = ""
-    for sinlge in blacklist_guanjianci:
-        filter_balck_ci += sinlge + "|"
-    filter_balck_ci = filter_balck_ci[0:len(filter_balck_ci)-1]
-    filter_data = filter_data[~filter_data['关键词'].str.contains(filter_balck_ci) ] 
     
-    # 查询出来的词包含某些字段
-    contains_ci = ""
-    for ci in  contains_cis:
-        contains_ci +=  ci + "|"
-    contains_ci = contains_ci[0:len(contains_ci)-1]
-    filter_data_bak =  filter_data.copy(deep=True)
-    filter_data = filter_data[filter_data['关键词'].str.contains(contains_ci)]
-    filter_data_bak_2 = filter_data.copy(deep=True)
-    filter_data_bak_2 = pandas.concat([filter_data_bak_2,filter_data_bak])
-    set_diff = filter_data_bak_2.drop_duplicates(subset=['关键词'],keep=False)
-    # 从这里筛选出来不同的词语,看是否有药摘出来的词语
-    set_diff.to_csv('./set_diff.csv')
-    filter_data.to_csv('result.csv')
-    print("运行结束")
+        # 过滤长度 
+        filter_data[filter_data['关键词'].str.len() > 20 ].to_excel(writer,sheet_name="关键词长度过滤")
+
+        filter_data = filter_data[filter_data['关键词'].str.len() <= 20 ] 
+        # 过滤某些词
+        filter_balck_ci = ""
+        for sinlge in blacklist_guanjianci:
+            filter_balck_ci += sinlge + "|"
+        filter_balck_ci = filter_balck_ci[0:len(filter_balck_ci)-1]
+        filter_data[filter_data['关键词'].str.contains(filter_balck_ci)].to_excel(writer,sheet_name="关键词过滤某些词")
+        filter_data = filter_data[~filter_data['关键词'].str.contains(filter_balck_ci) ] 
+        
+        # 查询出来的词包含某些字段
+        contains_ci = ""
+        for ci in  contains_cis:
+            contains_ci +=  ci + "|"
+        contains_ci = contains_ci[0:len(contains_ci)-1]
+        filter_data_bak =  filter_data.copy(deep=True)
+        filter_data[~filter_data['关键词'].str.contains(contains_ci)].to_excel(writer,sheet_name="包含的词过滤的某些内容")
+        filter_data = filter_data[filter_data['关键词'].str.contains(contains_ci)]
+        filter_data.drop_duplicates('关键词',inplace=True)
+        filter_data.to_excel(writer,'结果值')
+        print(filter_data)
+        print("运行结束")
 
     
-def run():
-    # csv_path ="/Users/ruiqi/data/code/notes/python/taobao/淘词参谋_22-16-20.xlsx"
-    # excel_data = pandas.read_csv(csv_path)
-    excel_csv_path ="/Users/ruiqi/data/code/notes/python/taobao/淘词参谋_13-57-13.xlsx"
+def run_hanfu():
+    #csv_path ="/Users/ruiqi/data/code/notes/python/taobao/淘词参谋_20-10-06.xlsx
+    #excel_data = pandas.read_csv(csv_path)
+    excel_csv_path ="/Users/ruiqi/data/code/notes/python/taobao/淘词参谋_21-07-33.xlsx"
     excel_data = pandas.read_excel(excel_csv_path)
-    exce_path = "/Users/ruiqi/data/code/notes/python/taobao/淘宝数据存储.xlsx"
+    exce_path = "/Users/ruiqi/data/code/notes/python/taobao/淘宝汉服存储.xlsx"
     blacklist = pandas.read_excel(exce_path,sheet_name=1)
     blacklist_guanjianci = pandas.read_excel(exce_path,sheet_name=2)
     contains_cis = pandas.read_excel(exce_path,sheet_name=3)
 
     filter_ci(excel_data,blacklist['过滤词'],blacklist_guanjianci['黑名单'],contains_cis['包含'])
+
+def run_xie():
+    excel_csv_path ="/Users/ruiqi/data/code/notes/python/taobao/淘词参谋_拖鞋.xlsx"
+    excel_data = pandas.read_excel(excel_csv_path)
+    exce_path = "/Users/ruiqi/data/code/notes/python/taobao/拖鞋词保存.xlsx"
+    blacklist = pandas.read_excel(exce_path,sheet_name=1)
+    blacklist_guanjianci = pandas.read_excel(exce_path,sheet_name=2)
+    contains_cis = pandas.read_excel(exce_path,sheet_name=3)
+
+    filter_ci(excel_data,blacklist['过滤词'],blacklist_guanjianci['黑名单'],contains_cis['包含'])
+    
+
 if __name__ == '__main__':
-    run()
+    run_hanfu()
    
